41367a0a56b85fcb392cf80363ab41d368d0da31
NyckoRBC1996 <nyckorbc1996@gmail.com>
2025-05-07 23:34:07 -0300

    Cambios en  controller que estaba llamando a los service en vez de las interfaces
    y algun cambio en los services

--- backend/ServiPuntos.API/Controllers/UsuarioController.cs
+++ backend/ServiPuntos.API/Controllers/UsuarioController.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[Route("api/usuario")]
+[Route("api/[controller]")]
 public class UsuarioController : ControllerBase
 {
-    private readonly UsuarioService _usuarioService;
+    private readonly IUsuarioService _iUsuarioService;
 
-    public UsuarioController(UsuarioService service)
+    public UsuarioController(IUsuarioService service)
     {
-        _usuarioService = service;
+        _iUsuarioService = service;
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(Guid id)
     {
-        var usuario = await _usuarioService.ObtenerPorId(id);
+        var usuario = await _iUsuarioService.GetUsuarioByIdAsync(id);
         return usuario is null ? NotFound() : Ok(usuario);
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll() => Ok(await _usuarioService.ListarUsuarios());
+    public async Task<IActionResult> GetAll() => Ok(await _iUsuarioService.GetAllUsuariosByTenantAsync());
 }
--- backend/ServiPuntos.Application/Services/UsuarioService.cs
+++ backend/ServiPuntos.Application/Services/UsuarioService.cs
@@ -6,36 +6,40 @@ public class UsuarioService : IUsuarioService
 
     private readonly ITenantResolver _iTenantResolver;
 
-    public UsuarioService(IUsuarioRepository usuarioRepository, ITenantResolver tenantResolver)
+    private readonly ITenantContext _tenantContext;
+
+    public UsuarioService(IUsuarioRepository usuarioRepository, ITenantResolver tenantResolver, ITenantContext tenantContext)
     {
         _iUsuarioRepository = usuarioRepository;
         _iTenantResolver = tenantResolver;
+        _tenantContext = tenantContext;
     }
 
     public async Task<Usuario?> GetUsuarioByIdAsync(Guid idUsuario)
     {
         return await _iUsuarioRepository.GetAsync(idUsuario);
     }
-    /*public async Task<IEnumerable<Usuario>> GetAllUsuariosAsync()
+    public async Task<IEnumerable<Usuario>> GetAllUsuariosAsync()
     {
-        return await _iUsuarioRepository.GetAllAsync();
-    }*/
+        var tenantId = _tenantContext.TenantId;
+        return await _iUsuarioRepository.GetAllByTenantAsync(tenantId);
+    }
     public async Task AddUsuarioAsync(Usuario usuario)
     {
-        var tenantId = _iTenantResolver.GetCurrentTenantId();
+        var tenantId = _tenantContext.TenantId;
         await _iUsuarioRepository.AddAsync(tenantId, usuario);
     }
     public async Task UpdateUsuarioAsync(Usuario usuario)
     {
-        var tenantId = _iTenantResolver.GetCurrentTenantId();
+        var tenantId = _tenantContext.TenantId;
         await _iUsuarioRepository.UpdateAsync(tenantId, usuario);
     }
     public async Task DeleteUsuarioAsync(Guid idUsuario)
     {
-        var tenantId = _iTenantResolver.GetCurrentTenantId();
+        var tenantId = _tenantContext.TenantId;
         await _iUsuarioRepository.DeleteAsync(tenantId, idUsuario);
     }
-    // Tenant como parametro
+    // Tenant como parametro (creo que ya no hace falta)
     public async Task AddUsuarioByTenantAsync(Guid tenantId, Usuario usuario)
     {
         await _iUsuarioRepository.AddAsync(tenantId, usuario);
