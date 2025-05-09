b77d5b170e8aa3e79e690e66ce26c0e02f8038c0
NyckoRBC1996 <nyckorbc1996@gmail.com>
2025-05-09 18:44:59 -0300

    Cambios program.cs de WebApp y de API

--- backend/ServiPuntos.WebApp/Program.cs
+++ backend/ServiPuntos.WebApp/Program.cs
@@ -1,18 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using ServiPuntos.Core.Interfaces;
 using ServiPuntos.Infrastructure.Data;
+using ServiPuntos.Infrastructure.MultiTenancy;
+using ServiPuntos.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
+//builder.Services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddScoped<ITenantService, TenantService>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 //builder.Services.AddScoped<IUbicacionService, UbicacionService>();
+builder.Services.AddScoped<ITenantRepository, TenantRepository>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+//builder.Services.AddScoped<IUbicacionRepository, UbicacionRepository>();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ITenantProvider, TenantProvider>();
+builder.Services.AddScoped<ITenantResolver, TenantResolver>();
+builder.Services.AddScoped<ITenantContext, TenantContext>();
 
 // Configuramos la conexion a la base de datos
+builder.Services.AddDbContext<TenantConfigurationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<ServiPuntosDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
