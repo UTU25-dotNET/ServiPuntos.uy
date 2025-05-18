f21f1768f0df11324e81e5ff573da7a650b5ac4c
NyckoRBC1996 <nyckorbc1996@gmail.com>
2025-05-17 23:11:54 -0300

    Cambios en Services , Interfaces y Repositorios

---
+++ backend/ServiPuntos.Application/Services/ProductoUbicacionService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiPuntos.Core.Entities;
+using ServiPuntos.Core.Interfaces;
+
+namespace ServiPuntos.Application.Services
+{
+    public class ProductoUbicacionService : IProductoUbicacionService
+    {
+        private readonly IProductoUbicacionRepository _iProductoUbicacionRepository;
+        private readonly ITenantResolver _iTenantResolver;
+
+        private readonly ITenantContext _iTenantContext;
+        public ProductoUbicacionService(IProductoUbicacionRepository productoUbicacionRepository, ITenantContext tenantContext, ITenantResolver tenantResolver)
+        {
+            _iProductoUbicacionRepository = productoUbicacionRepository;
+            _iTenantContext = tenantContext;
+            _iTenantResolver = tenantResolver;
+        }
+
+        public async Task<IEnumerable<ProductoUbicacion>> GetAllAsync()
+        {
+            return await _iProductoUbicacionRepository.GetAllAsync();
+        }
+        public async Task<ProductoUbicacion?> GetAsync(Guid id)
+        {
+            return await _iProductoUbicacionRepository.GetAsync(id);
+        }
+        public async Task AddAsync(ProductoUbicacion productoUbicacion)
+        {
+            await _iProductoUbicacionRepository.AddAsync(productoUbicacion);
+        }
+        public async Task UpdateAsync(ProductoUbicacion productoUbicacion)
+        {
+            await _iProductoUbicacionRepository.UpdateAsync(productoUbicacion);
+        }
+
+    }
+}
---
+++ backend/ServiPuntos.Core/Interfaces/IProductoUbicacionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiPuntos.Core.Entities;
+
+namespace ServiPuntos.Core.Interfaces
+{
+    public interface IProductoUbicacionRepository
+    {
+        Task<ProductoUbicacion?> GetAsync(Guid id);
+        //Task<IEnumerable<ProductoUbicacion>> GetAllAsync();
+        Task AddAsync(ProductoUbicacion productoUbicacion);
+        Task UpdateAsync(ProductoUbicacion productoUbicacion);
+    }
+}
