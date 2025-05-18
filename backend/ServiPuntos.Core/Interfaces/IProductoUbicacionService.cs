7c615a899090aad7473c9b572a3640eece72a296
NyckoRBC1996 <nyckorbc1996@gmail.com>
2025-05-17 23:11:54 -0300

    Cambios en Services , Interfaces y Repositorios

---
+++ backend/ServiPuntos.Core/Interfaces/IProductoUbicacionService.cs
@@ -0,0 +1,12 @@
+﻿using ServiPuntos.Core.Entities;
+
+namespace ServiPuntos.Core.Interfaces
+{
+    public interface IProductoUbicacionService
+    {
+        Task<IEnumerable<ProductoUbicacion>> GetAllAsync();
+        Task<ProductoUbicacion?> GetAsync(Guid id);
+        Task AddAsync(ProductoUbicacion productoUbicacion);
+        Task UpdateAsync(ProductoUbicacion productoUbicacion);
+    }
+}
---
+++ backend/ServiPuntos.Infrastructure/Repositories/ProductoUbicacionRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiPuntos.Core.Entities;
+using ServiPuntos.Core.Interfaces;
+using ServiPuntos.Infrastructure.Data;
+
+namespace ServiPuntos.Infrastructure.Repositories
+{
+    public class ProductoUbicacionRepository : IProductoUbicacionRepository
+    {
+        private readonly ServiPuntosDbContext _dbContext;
+        public ProductoUbicacionRepository(ServiPuntosDbContext context)
+        {
+            _dbContext = context;
+        }
+        public Task<ProductoUbicacion?> GetAsync(Guid id)
+            => _dbContext.ProductoUbicaciones
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+        /*public Task<IEnumerable<ProductoUbicacion>> GetAllAsync()
+            => _dbContext.ProductoUbicaciones
+                .ToListAsync();*/
+        public Task AddAsync(ProductoUbicacion productoUbicacion)
+        {
+            _dbContext.ProductoUbicaciones.Add(productoUbicacion);
+            return _dbContext.SaveChangesAsync();
+        }
+        public Task UpdateAsync(ProductoUbicacion productoUbicacion)
+        {
+            _dbContext.ProductoUbicaciones.Update(productoUbicacion);
+            return _dbContext.SaveChangesAsync();
+        }
+
+    }
+}
