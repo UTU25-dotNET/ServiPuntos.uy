5948605061a4ca783c07a20c228d02fb3c863e5b
NyckoRBC1996 <nyckorbc1996@gmail.com>
2025-06-04 20:29:15 -0300

    Migraciones

---
+++ backend/ServiPuntos.Infrastructure/Migrations/20250604224456_MigracionFecha2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiPuntos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionFecha2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FechaDeModificacion",
+                table: "Ubicaciones",
+                newName: "FechaModificacion");
+
+            migrationBuilder.RenameColumn(
+                name: "FechaDeCreacion",
+                table: "Ubicaciones",
+                newName: "FechaCreacion");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FechaModificacion",
+                table: "Ubicaciones",
+                newName: "FechaDeModificacion");
+
+            migrationBuilder.RenameColumn(
+                name: "FechaCreacion",
+                table: "Ubicaciones",
+                newName: "FechaDeCreacion");
+        }
+    }
+}
