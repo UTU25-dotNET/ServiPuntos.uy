4f6180b8b42e827a149f2d5a9410481f3a59bcf0
Diego Pereyra <diegopirazabal@gmail.com>
2025-06-09 14:19:33 -0300

    Funcionalidades de AdminUbicacion

---
+++ backend/ServiPuntos.Infrastructure/Migrations/20250609171850_MigracionPrecioServiciosFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiPuntos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionPrecioServiciosFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
