5a0a9a61f530d88f8d974c26edc1d55613499220
bentanic-0 <bentanico@gmail.com>
2025-06-12 01:13:53 -0300

    Add VeaiResponse model for handling response data

---
+++ mobile/ServiPuntos.Mobile/Models/VeaiResponse.cs
@@ -0,0 +1,9 @@
+namespace ServiPuntos.Mobile.Models
+{
+    public class VeaiResponse
+    {
+        public bool IsAllowed { get; set; }
+        public int Edad { get; set; }
+        public string? Message { get; set; }
+    }
+}
