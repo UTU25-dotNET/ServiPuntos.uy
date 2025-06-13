51338a2118e496d556a9d87ed3f347c0c9b88dee
bentanic-0 <bentanico@gmail.com>
2025-06-13 13:55:45 -0300

    Implement IProductoService interface and enhance ProductoService with methods for retrieving products by location and stock availability

--- mobile/ServiPuntos.Mobile/Services/ProductoService.cs
+++ mobile/ServiPuntos.Mobile/Services/ProductoService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ using ServiPuntos.Mobile.Models;
 
 namespace ServiPuntos.Mobile.Services
 {
+    public interface IProductoService
+    {
+        Task<List<ProductoUbicacionDto>> GetProductosPorUbicacionAsync(string ubicacionId);
+        Task<int> GetStockAsync(string ubicacionId, string productoId);
+    }
+
     public class ProductoService : IProductoService
     {
         private readonly HttpClient _httpClient;
@@ -16,26 +23,50 @@ namespace ServiPuntos.Mobile.Services
             _httpClient = httpClient;
         }
 
-        public async Task<List<ProductoCanjeableDto>> GetProductosPorUbicacionAsync(string ubicacionId)
+
+        public async Task<List<ProductoUbicacionDto>> GetProductosPorUbicacionAsync(string ubicacionId)
         {
-
-            var list = await _httpClient.GetFromJsonAsync<List<ProductoCanjeableDto>>(
-                $"ubicacion/{ubicacionId}/productos");
-
-
-            return list ?? new List<ProductoCanjeableDto>();
+            var list = await _httpClient.GetFromJsonAsync<List<ProductoUbicacionDto>>($"ubicacion/{ubicacionId}");
+            return list ?? new List<ProductoUbicacionDto>();
         }
+
 
         public async Task<int> GetStockAsync(string ubicacionId, string productoId)
         {
 
-            var resp = await _httpClient.GetAsync(
-                $"ubicacion/{ubicacionId}/stock/{productoId}");
+            var resp = await _httpClient.GetAsync($"ubicacion/{ubicacionId}/stock");
             resp.EnsureSuccessStatusCode();
+            var stockData = await resp.Content.ReadFromJsonAsync<UbicacionStockResponse>();
 
-
-            var stock = await resp.Content.ReadFromJsonAsync<int?>();
-            return stock.GetValueOrDefault();
+            var producto = stockData?.ProductosStock?.FirstOrDefault(p => p.ProductoId.ToString() == productoId);
+            return producto?.StockDisponible ?? 0;
         }
+    }
+
+
+    public class ProductoUbicacionDto
+    {
+        public string Id { get; set; }
+        public string UbicacionId { get; set; }
+        public string ProductoCanjeableId { get; set; }
+        public string ProductoNombre { get; set; }
+        public int StockDisponible { get; set; }
+        public bool Activo { get; set; }
+    }
+
+    public class UbicacionStockResponse
+    {
+        public string UbicacionId { get; set; }
+        public string UbicacionNombre { get; set; }
+        public List<ProductoStockDto> ProductosStock { get; set; }
+    }
+
+    public class ProductoStockDto
+    {
+        public string Id { get; set; }
+        public string ProductoId { get; set; }
+        public string ProductoNombre { get; set; }
+        public int StockDisponible { get; set; }
+        public bool Activo { get; set; }
     }
 }
