ead85ed5b82dbe0c0538c938837088b0caefad26
Diego Pereyra <diegopirazabal@gmail.com>
2025-06-13 11:48:03 -0300

    transaccion aprobada paypal

--- backend/ServiPuntos.API/Controllers/PayPalController.cs
+++ backend/ServiPuntos.API/Controllers/PayPalController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using ServiPuntos.Core.Interfaces;
+using ServiPuntos.Core.NAFTA;
 
 namespace ServiPuntos.API.Controllers
 {
@@ -7,12 +9,32 @@ namespace ServiPuntos.API.Controllers
     [Route("api/paypal")]
     public class PayPalController : ControllerBase
     {
+        private readonly INAFTAService _naftaService;
+
+        public PayPalController(INAFTAService naftaService)
+        {
+            _naftaService = naftaService;
+        }
+
         [HttpGet("return")]
-        public IActionResult PayPalReturn([FromQuery] string paymentId, [FromQuery] string PayerID, [FromQuery] string token)
+
+        public async Task<IActionResult> PayPalReturn([FromQuery] string paymentId, [FromQuery] string PayerID, [FromQuery] string token)
+        {
+            // Cuando el usuario aprueba el pago en PayPal, confirmamos la transacción
+            var mensaje = new MensajeNAFTA
         {
             // Esta URL se llama cuando el usuario APRUEBA el pago en PayPal
-            var redirectUrl = $"http://localhost:3000/paypal-return?paymentId={Uri.EscapeDataString(paymentId)}&payerId={Uri.EscapeDataString(PayerID)}&token={Uri.EscapeDataString(token)}";
-            return Redirect(redirectUrl);
+            TipoMensaje = Core.Enums.TipoMensajeNAFTA.Transaccion,
+                Datos = new System.Collections.Generic.Dictionary<string, object>
+                {
+                    { "paymentId", paymentId },
+                    { "payerId", PayerID },
+                    { "token", token }
+                }
+            };
+
+            var respuesta = await _naftaService.ConfirmarPagoPayPalAsync(mensaje);
+            return Ok(respuesta);
         }
 
         [HttpGet("cancel")]
