8fa3fb8cc0e848ac396ff3aed34340fd91db6bff
Juampa Romero <juamparomero@192.168.0.197>
2025-06-17 18:15:48 -0300

    URL actualizadas apuntando a AWS

--- backend/ServiPuntos.API/Controllers/AuthController.cs
+++ backend/ServiPuntos.API/Controllers/AuthController.cs
@@ -122,9 +122,9 @@ public class AuthController : ControllerBase
         {
             throw new InvalidOperationException("Google ClientId is not configured.");
         }
-         var redirectUri = "https://localhost:5019/api/auth/google-callback";
+        //var redirectUri = "https://localhost:5019/api/auth/google-callback";
         //var redirectUri = "https://servipuntos-api.duckdns.org/api/auth/google-callback";
-        //var redirectUri = "https://ec2-18-220-251-96.us-east-2.compute.amazonaws.com:5019/api/auth/google-callback";
+        var redirectUri = "https://ec2-18-220-251-96.us-east-2.compute.amazonaws.com:5019/api/auth/google-callback";
 
         var scope = "email profile openid";
 
@@ -224,9 +224,9 @@ public class AuthController : ControllerBase
             {
                 throw new InvalidOperationException("Google ClientId or ClientSecret is not configured.");
             }
-            var redirectUri = "https://localhost:5019/api/auth/google-callback";
+            //redirectUri = "https://localhost:5019/api/auth/google-callback";
             //var redirectUri = " https://servipuntos-api.duckdns.org:5019/api/auth/google-callback";
-            //var redirectUri = "https://ec2-18-220-251-96.us-east-2.compute.amazonaws.com:5019/api/auth/google-callback";
+            var redirectUri = "https://ec2-18-220-251-96.us-east-2.compute.amazonaws.com:5019/api/auth/google-callback";
             var code_verifier = HttpContext.Session.GetString("CodeVerifier");
             Console.WriteLine($"[GoogleCallback] Code Verifier recuperado de sesión: {code_verifier}");
             if (string.IsNullOrEmpty(code_verifier))
