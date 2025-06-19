e40931253ef81ae0907f807875fdc350a8ebe591
bentanic-0 <bentanico@gmail.com>
2025-06-18 23:45:30 -0300

    feat(app): add HistoryPage to AppShell and register TokenDisplayPage route

--- mobile/ServiPuntos.Mobile/AppShell.xaml.cs
+++ mobile/ServiPuntos.Mobile/AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace ServiPuntos.Mobile;
+﻿using ServiPuntos.Mobile.Views;
+
+namespace ServiPuntos.Mobile;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+		Routing.RegisterRoute(nameof(TokenDisplayPage), typeof(TokenDisplayPage));
 	}
 }
