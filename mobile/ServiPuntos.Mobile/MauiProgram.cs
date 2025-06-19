b10a0562571475e872e384ae3a6f0aeeccc7e147
bentanic-0 <bentanico@gmail.com>
2025-06-18 23:45:55 -0300

    feat(app): add HistoryService and HistoryPage to the application

--- mobile/ServiPuntos.Mobile/MauiProgram.cs
+++ mobile/ServiPuntos.Mobile/MauiProgram.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Maui;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
 using ServiPuntos.Mobile.Handlers;
@@ -25,34 +24,44 @@ namespace ServiPuntos.Mobile
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-
             builder.Services.AddSingleton<IAuthService, AuthService>();
             builder.Services.AddSingleton<LoginViewModel>();
             builder.Services.AddSingleton<LoginPage>();
             builder.Services.AddSingleton<TokenDisplayPage>();
 
-
             builder.Services.AddTransient<AuthMessageHandler>();
-
 
             builder.Services
                 .AddHttpClient<IPointsService, PointsService>(client =>
                 {
                     client.BaseAddress = new Uri("https://ec2-18-220-251-96.us-east-2.compute.amazonaws.com:5019/");
                 })
-
                 .ConfigurePrimaryHttpMessageHandler(() =>
                     new HttpClientHandler
                     {
                         ServerCertificateCustomValidationCallback =
                             HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
                     })
-
                 .AddHttpMessageHandler<AuthMessageHandler>();
-
 
             builder.Services.AddSingleton<PointsViewModel>();
             builder.Services.AddSingleton<PointsPage>();
+
+            builder.Services
+                .AddHttpClient<IHistoryService, HistoryService>(client =>
+                {
+                    client.BaseAddress = new Uri("https://ec2-18-220-251-96.us-east-2.compute.amazonaws.com:5019/");
+                })
+                .ConfigurePrimaryHttpMessageHandler(() =>
+                    new HttpClientHandler
+                    {
+                        ServerCertificateCustomValidationCallback =
+                            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                    })
+                .AddHttpMessageHandler<AuthMessageHandler>();
+
+            builder.Services.AddSingleton<HistoryViewModel>();
+            builder.Services.AddSingleton<HistoryPage>();
 
 #if DEBUG
             builder.Logging.AddDebug();
