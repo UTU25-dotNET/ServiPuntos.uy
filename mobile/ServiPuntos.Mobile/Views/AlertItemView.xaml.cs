1278788f4458b770d91de9214959311f31a68cfd
bentanic-0 <bentanico@gmail.com>
2025-07-08 18:21:05 -0300

    Add AlertItemView for displaying alert messages

---
+++ mobile/ServiPuntos.Mobile/Views/AlertItemView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace ServiPuntos.Mobile.Views;
+
+public partial class AlertItemView : ContentView
+{
+    public AlertItemView()
+    {
+        InitializeComponent();
+    }
+}
