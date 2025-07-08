03afc816aab673f2438b18ce87898d786c96f1c4
bentanic-0 <bentanico@gmail.com>
2025-07-08 18:19:56 -0300

    Add MessagingConstants for user login/logout events

---
+++ mobile/ServiPuntos.Mobile/Helpers/MessagingConstants.cs
@@ -0,0 +1,8 @@
+namespace ServiPuntos.Mobile.Helpers
+{
+    public static class MessagingConstants
+    {
+        public const string UserLoggedIn = "UserLoggedIn";
+        public const string UserLoggedOut = "UserLoggedOut";
+    }
+}
