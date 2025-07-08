51f21e74cb936181350fab0cc586e3f0a9016760
bentanic-0 <bentanico@gmail.com>
2025-07-08 18:25:29 -0300

    Add TransactionItemView for displaying transaction details in a structured format

---
+++ mobile/ServiPuntos.Mobile/Views/TransactionItemView.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace ServiPuntos.Mobile.Views
+{
+    public partial class TransactionItemView : ContentView
+    {
+        public TransactionItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
